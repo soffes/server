c39d75fa2ec0c9a00946831bf9b23e1599c7b412
Sam Soffes <sam@soff.es>
2015-04-27 12:48:43 -0700

    Ruby is hard

--- solo.rb
+++ solo.rb
@@ -3,4 +3,4 @@ cookbook_path [
   File.expand_path('../berks-cookbooks', __FILE__)
 ]
 json_attribs File.expand_path('../node.json', __FILE__)
-data_bags_path File.expand_path('../data_bags', __FILE__),
+data_bags_path File.expand_path('../data_bags', __FILE__)
